feb24da9ee45be5d22e92bb089d7973116c017e1
tripplesixblunt <leonardo02lima@gmail.com>
2021-04-21 18:22:22 -0300

    Alguns novos elementos adicionado e refatorados

--- App.tsx
+++ App.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import AppLoading from 'expo-app-loading'
+
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from '@expo-google-fonts/jost'
+
 import { Welcome } from './src/views/Welcome'
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <Welcome/>
   );
--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,35 +1,41 @@
-import React, { useState } from 'react'
-import { Text, View, Image, StyleSheet, SafeAreaView, StatusBar } from 'react-native'
+import React from 'react'
+import { Text, 
+         View, 
+         Image, 
+         StyleSheet, 
+         SafeAreaView,
+         TouchableOpacity, 
+         Dimensions,
+         StatusBar } from 'react-native'
+import { FontAwesome5 } from '@expo/vector-icons'
 
 import  wateringImg  from '../assets/watering.png'
 
-import colors from '../styles/colors';
-import { Button } from '../components/Button'
+import colors from '../styles/colors'
+import fonts from '../styles/fonts'
+
 
 export function Welcome(){
-  // [] um vetor com duas posição, que ira armazenar o conteudo do estado
-  // visible: que fica com o valor do elemento
-  // setVisible: função que atualiza esse valor do elemento(estado)
-  const [visible, setVisible] = useState(false);
-
-  function handleVisibility(){
-    setVisible(true);
-  }
   return(
     <SafeAreaView style={styles.constainer}>
-      <StatusBar/>
-      <Text style={styles.title}>Gerencie {'\n'} suas plantas {'\n'} de forma fácil</Text>
+      <View style={styles.wrapper}>
+        <StatusBar/>
+        <Text style={styles.title}>Gerencie {'\n'} suas plantas de{'\n'} forma fácil</Text>
 
-      {
-        visible &&
-        <Image source={wateringImg} style={styles.image}/>
-      
-      }
+        <Image source={wateringImg} 
+              style={styles.image}
+              resizeMode="contain"
+        />
+        
+        <Text style={styles.subtitle}>Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+              sempre que precisar.</Text>
 
-      <Text style={styles.subtitle}>Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
-            sempre que precisar.</Text>
-
-      <Button title=">"/>      
+        <TouchableOpacity style={styles.button} 
+                        activeOpacity={0.7}>
+            <FontAwesome5 name='chevron-right'
+                          style={styles.buttonIcon}/>
+        </TouchableOpacity>   
+      </View> 
       
     </SafeAreaView>
   );
@@ -38,25 +44,45 @@ export function Welcome(){
 const styles = StyleSheet.create({
   constainer:{
     flex:1,
+    
+  },
+  wrapper:{
+    flex:1,
     alignItems:'center',
-    justifyContent:'space-between',
+    justifyContent:'space-around',
+    paddingHorizontal:20,
   },
   title:{
-    fontSize:32,
-    fontWeight:'800',
+    fontSize:28,
+    fontWeight:'bold',
     textAlign:'center',
     color: colors.heading,
+    fontFamily:fonts.heading,
+    lineHeight:34,
     marginTop:38,
   },
   subtitle:{
     textAlign:'center',
     fontSize:18,
     paddingHorizontal:20,
-    color:colors.heading
+    color:colors.heading,
+    fontFamily:fonts.text
   },
   image:{
-    width:292,
-    height:284,
+    height: Dimensions.get('window').width * 0.7
+  },
+  button:{
+    backgroundColor: colors.green,
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:16,
+    marginBottom:10,
+    width:56,
+    height:56,
+  },
+  buttonIcon:{
+    color: colors.white,
+    fontSize:24,
   },
  
 });
